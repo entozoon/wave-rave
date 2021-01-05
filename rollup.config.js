32d9918d897e72ca3f0065ca6e0ba85666cfaff7
Myke <michael.cook@techdept.co.uk>
2021-01-05 19:38:19 +0000

    Yep, phaser 4 is not ready.

---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+import filesize from 'rollup-plugin-filesize';
+import image from '@rollup/plugin-image';
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+
+    input: './src/main.ts',
+
+    output: [
+        {
+            file: 'public/bundle.js',
+            format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+            name: 'Phaser4Game',
+            sourcemap: true
+        }
+    ],
+
+    plugins: [
+
+        image({
+            //  If true, instructs the plugin to generate an ES Module which exports a DOM Image
+            //  If false, plugin generates base64 data
+            dom: false
+        }),
+
+        resolve({
+            extensions: [ '.js', '.jsx', '.ts', '.tsx' ]
+        }),
+
+        typescript({
+            tsconfig: './tsconfig.json'
+        }),
+
+        filesize(),
+
+        // minify, but only in production, not in watch mode
+        production && terser()
+
+    ]
+
+};
